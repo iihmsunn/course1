6c49e681817d384f79dcf910ba0062794fb37e3c
rjawiygvozd <rjawiygvozd@gmail.com>
2013-02-12 16:25:25 +0700

    test test

---
+++ testtest.py
@@ -0,0 +1,10 @@
+import unittest
+class Test(unittest.TestCase):
+    def test(self):
+        print('Luck!')
+    def test2(self):
+        print('Fuck!')
+        assert False
+        
+        
+    
